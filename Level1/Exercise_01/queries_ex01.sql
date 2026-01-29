1ae858ab678f7b8e8655115c776c814ac93356e2
Federico Cantore <f.cantore85@gmail.com>
2026-01-29 13:19:16 +0100

    Update queries_ex01.sql

--- Level1/Exercise_01/queries_ex01.sql
+++ Level1/Exercise_01/queries_ex01.sql
@@ -2,33 +2,41 @@
 SELECT
     c.customer_name  AS client,
     COUNT(s.sale_id) AS total_invoices
+    SUM(s.total_price) AS total_import
 FROM customers c
 JOIN sales s
 ON c.customer_id = s.customer_id
 WHERE c.customer_name = 'Marcos Rodriguez Torres' AND s.sale_date BETWEEN '2020-12-01' AND '2025-12-18'
-GROUP BY c.customer_name;
+GROUP BY c.customer_id, c.customer_name;
 
 SELECT
     e.employee_name AS employee,
-    g.brand         AS glass_brand,
-    s.sale_date     AS day
+    b.name          AS glass_brand,
+    g.mount_type    AS mount_type,
+    g.mount_color   AS mount_color,
+    s.sale_date     AS sale_date
 FROM sales s
-INNER JOIN employees e
+JOIN employees e
 ON s.employee_id = e.employee_id
-INNER JOIN glasses g
+JOIN glasses g
 ON s.glass_id = g.glass_id
-WHERE e.employee_name = 'Luisa Martínez Ruiz' AND s.sale_date BETWEEN '2025-01-01' AND '2025-12-31'
+JOIN brands b 
+ON g.brand_id = b.brand_id
+WHERE e.employee_name = 'Luisa Martínez Ruiz' AND YEAR(s.sale_date) = 2025
 ORDER BY s.sale_date DESC;
 
 SELECT
-    sup.supplier_name AS provider,
-    g.brand           AS brand,
-    SUM(s.quantity)   AS glasses_sold
+    sup.supplier_name         AS provider,
+    COUNT(DISTINC b.brand_id) AS total_brands_sells,
+    SUM(s.quantity)           AS total_unity_sells,
+    SUM(s.total_price)        AS total_invoces
 FROM sales s
-    INNER JOIN glasses g
-    ON s.glass_id = g.glass_id
-    INNER JOIN suppliers sup
-    ON g.supplier_id = sup.supplier_id
-GROUP BY sup.supplier_name, g.brand
-ORDER BY glasses_sold DESC;
+JOIN glasses g
+ON s.glass_id = g.glass_id
+JOIN brands b 
+ON g.brand_id = b.brand_id
+JOIN suppliers sup
+ON g.supplier_id = sup.supplier_id
+GROUP BY sup.supplier_id, sup.supplier_name
+ORDER BY total_invoces DESC;
 
