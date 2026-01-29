56958bb31ac73bdc789cc7cbbe3d6de0e8a8c24c
Federico Cantore <f.cantore85@gmail.com>
2026-01-29 13:24:19 +0100

    Update queries_ex01.sql

--- Level1/Exercise_01/queries_ex01.sql
+++ Level1/Exercise_01/queries_ex01.sql
@@ -1,7 +1,7 @@
 
 SELECT
     c.customer_name  AS client,
-    COUNT(s.sale_id) AS total_invoices
+    COUNT(s.sale_id) AS total_invoices,
     SUM(s.total_price) AS total_import
 FROM customers c
 JOIN sales s
@@ -27,16 +27,16 @@ ORDER BY s.sale_date DESC;
 
 SELECT
     sup.supplier_name         AS provider,
-    COUNT(DISTINC b.brand_id) AS total_brands_sells,
+    COUNT(DISTINCT b.brand_id) AS total_brands_sells,
     SUM(s.quantity)           AS total_unity_sells,
-    SUM(s.total_price)        AS total_invoces
+    SUM(s.total_price)        AS total_invoices
 FROM sales s
 JOIN glasses g
 ON s.glass_id = g.glass_id
 JOIN brands b 
 ON g.brand_id = b.brand_id
 JOIN suppliers sup
-ON g.supplier_id = sup.supplier_id
+ON b.supplier_id = sup.supplier_id
 GROUP BY sup.supplier_id, sup.supplier_name
 ORDER BY total_invoces DESC;
 
